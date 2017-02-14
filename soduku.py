1b19c449f35993f18c15521157822033a5d17629
chaowan2017 <chaowan_2017@depauw.edu>
2017-02-14 00:29:58 -0500

    finished
    finished

--- soduku.py
+++ soduku.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 def make_board(m=3):
 	numbers = list(range(1, m**2 + 1 ))
-	
+
 	board = None
 	while board is None:
 		board = attempt_board(m, numbers)
@@ -57,8 +57,6 @@ def print_answers(board):
 		if (i+1)%3 == 0 : print(spacer.replace('-','='))
 		else: print(spacer)
 		
-Board = make_board()
-print_board(Board)
 
 def is_full(challange, m = 3 ):
 	for i,j in itertools.product(range(m**2), repeat= 2):
@@ -66,3 +64,77 @@ def is_full(challange, m = 3 ):
 			return False
 	return True
 
+def cal_candidate(challange, x, y, m=3):
+
+	candidate = range(1, m ** 2 + 1)
+	for i in range(m**2):
+		if challange[x][i] in candidate:
+			candidate.remove(challange[x][i])
+		if challange[i][y] in candidate:
+			candidate.remove(challange[i][y])
+
+	for i,j in itertools.product(range(m), repeat=2):
+		x0, y0 = x - x % m, y - y % m
+		if challange[x0+i][y0+j] in candidate:
+			candidate.remove(challange[x0+i][y0+j])
+
+	return candidate
+
+def least_candidate(challange, m=3):
+	least, x, y = m ** 2 , -1, -1
+	for i, j in itertools.product(range(m**2), repeat=2):
+		if not challange[i][j]:
+			num = len(cal_candidate(challange, i, j))
+			if num < least:
+				least = num
+				x, y = i,j
+	return x,y
+
+def solving_soduku(challange, m=3):
+	if is_full(challange):
+		return challange
+
+	x, y = least_candidate(challange)
+	id = x * (m**2)+y
+	result = try_candidate(challange, id)
+	return result
+
+def try_candidate(challange, id, m=3):
+
+	if is_full(challange):
+		return challange
+	x = id/(m**2)
+	y = id%(m**2)
+
+	while challange[x][y]:
+		id = (id + 1) % m**4
+		x = id / (m**2)
+		y = id % (m**2)
+	candidate = cal_candidate(challange, x, y)
+
+	if len(candidate) == 0:
+		return False
+
+	for i in range(len(candidate)):
+		challange[x][y] = candidate[i]
+		result_r = try_candidate(challange, (id + 1) % m ** 4)
+		if not result_r:
+			pass
+		else:
+			return challange
+
+	challange[x][y]= None
+	return False
+
+
+Input = [[8,None,None,None,None,None,None,None,None],
+           [None,None,3,6,None,None,None,None,None],
+           [None, 7,None,None,9,None,2,None,None],
+           [None,5,None,None,None,7,None,None,None],
+           [None,None,None,None,4,5,7,None,None],
+           [None,None,None,1,None,None,None,3,None],
+           [None,None,1,None,None,None,None,6,8],
+           [None,None,8,5,None,None,None,1,None],
+           [None,9,None,None,None,None,4,None,None]]
+result = solving_soduku(Input)
+print_answers(result)
